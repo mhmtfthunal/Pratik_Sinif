f46a9c571a13993a47575a195e3c45e901e14c29
FatihNitro <FatihNitro@DESKTOP-KERR77G>
2025-06-12 13:48:28 +0300

    Add project files.

---
+++ Pratik_Sinif/Models/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pratik_Sinif.Models
+{
+    internal class Person
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int BirthDay { get; set; }
+    }
+}
---
+++ Pratik_Sinif/Program.cs
@@ -0,0 +1,19 @@
+﻿using Pratik_Sinif.Models;
+
+Teacher teacher = new Teacher()
+{
+    Name = "Sinan",
+    SurName = "Ferza",
+    BirthDay = 1981
+
+};
+
+Student student = new Student()
+{
+    Name = "Selim",
+    SurName = "Bezgin",
+    BirthDay = 1998
+};
+
+Console.WriteLine("Öğretmenimiz " + teacher.Name + " " + "derslerinde başarılı örnekler kullanmaktadır.");
+Console.WriteLine("Öğrencilerimizden de " + student.Name + " "+ student.BirthDay + " "+ "yılında doğduğunu anlattı.");
